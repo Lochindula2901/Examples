f90e3f8b289c20a0c5844c118c94ab006e1605de
Natalya Fiklistova <chost2009@mail.ru>
2023-09-23 19:58:44 +0200

    Homework

--- example_6/Program.cs
+++ example_6/Program.cs
@@ -1,10 +1,44 @@
-﻿int a = 5;
-int b = 7;
+﻿//int a = 5;
+//int b = 7;
+
+//int max = a;
+
+//if (a > max ) max = a;
+//if (b > max ) max = b;
+
+//Console.Write("max = ");
+//Console.WriteLine(max);
+
+//int a = 2;
+//int b = 10;
+
+//int  max = a;
+//if (a > max ) max = a;
+//if (b > max ) max = b;
+
+//Console.Write("max = ");
+//Console.WriteLine(max);
+
+//int a = -3;
+//int b = -9;
+ 
+//int max = a; 
+
+//if (a > max ) max = a;
+//if (b > max ) max = b;
+
+//Console.Write("max = ");
+//Console.WriteLine(max);
+
+int a = 2;
+int b = 3;
+int c = 7;
 
 int max = a;
-if (a > max ) max = a;
-if (b > max ) max = b;
+
+if (a > max)max = a;
+if (b > max)max = b;
+if (c > max)max = c;
 
 Console.Write("max = ");
 Console.WriteLine(max);
-
