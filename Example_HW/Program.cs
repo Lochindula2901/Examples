cdc3a2b22235b9497bd2945b0195dbb978e3a6ca
Natalya Fiklistova <chost2009@mail.ru>
2023-10-08 15:56:34 +0200

    dd

--- Example_HW/Program.cs
+++ Example_HW/Program.cs
@@ -5,6 +5,24 @@
 //Console.WriteLine(result);
 
 
-int numberN = new Random().Next(1, N); //1234....N
-Console.WriteLine(numberN);
+//int numberN = new Random().Next(1, N); //1234....N
+//Console.WriteLine(numberN);
+
+
+//using System.Transactions;
+
+//Console.WriteLine("enter int number" );
+//int number = Convert.ToInt32(Console.ReadLine());
+
+//int square = number * number;
+//Console.WriteLine(number + "->" + square);
+
+Console.WriteLine("enter int numberA");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("enter int numberB");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int squareTwo = numberA / numberB;
+Console.WriteLine( "->" + squareTwo);
 
--- example_11/Program.cs
+++ example_11/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Дан текст 
+//все пробелы заменить на черточки, 
+//маленькие буквы к на большие К
+//А большие с на маленькие С
+
+
+
+
+string text = "Прошло семь лет после 12-го года. "
+            + "Взволнованное историческое море Европы улеглось в свои берега. "
+            + "Оно казалось затихшим; но таинственные силы," 
+            + "двигающие человечество, продолжали свое действие.";
+
+//string s = "qwerty"
+//            012345
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = string.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+string newText = Replace(text, ' ', '|');
+
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = Replace(newText, 'с', 'С');
+Console.WriteLine(newText);
