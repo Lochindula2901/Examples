3939463f4fb0403412aef57d74324928f4f9301a
Natalya Fiklistova <chost2009@mail.ru>
2023-10-09 15:28:58 +0200

    Дз29

---
+++ example29/Program.cs
@@ -0,0 +1,17 @@
+﻿/* 
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] */
+
+int N = 8;
+int[] SetArray(int x)
+{
+    int[] arr = new int[x];
+    for (int i = 0; i < x; i++)
+    {
+        arr[i] = new Random().Next(1, 100);
+    }
+    return arr;
+}
+
+Console.WriteLine('[' + string.Join(", ", SetArray(N)) + ']');
