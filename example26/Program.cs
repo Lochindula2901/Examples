30352c29b043c3735cc6f542f088a3623a4543d7
Natalya Fiklistova <chost2009@mail.ru>
2023-10-09 15:20:17 +0200

    Дз26

--- example26/Program.cs
+++ example26/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int N = 8;
+int[] SetArray(int x)
+{
+    int[] arr = new int[x];
+    for (int i = 0; i < x; i++)
+    {
+        arr[i] = new Random().Next(1, 100);
+    }
+    return arr;
+}
+
+Console.WriteLine('[' + string.Join(", ", SetArray(N)) + ']');
+
