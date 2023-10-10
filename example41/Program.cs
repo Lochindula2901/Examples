3ebeaaf17baa46096f71dd782de2046f0c7f0aa2
Natalya Fiklistova <chost2009@mail.ru>
2023-10-10 19:50:45 +0200

    Дз 41

--- example41/Program.cs
+++ example41/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+Console.Write($"Введите число М(количество чисел): ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] arrayNumbersM = new int[m];
+
+void EnteringNumber(int m)
+{
+    for (int i = 0; i < m; i++)
+    {
+        Console.Write($"Введите {i + 1} число: ");
+        arrayNumbersM[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+
+int Comparison(int[] arrayN)
+{
+    int count = 0;
+    for (int i = 0; i < arrayN.Length; i++)
+    {
+        if (arrayN[i] > 0) count += 1;
+    }
+    return count;
+}
+
+EnteringNumber(m);
+
+Console.WriteLine($"Введено чисел больше '0':  {Comparison(arrayNumbersM)} ");
