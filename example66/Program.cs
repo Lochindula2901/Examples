297307c3df29b6f5c28d25ddf54e3577dd51e48e
Natalya Fiklistova <chost2009@mail.ru>
2023-10-20 04:58:42 +0200

    HW66

--- example66/Program.cs
+++ example66/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+Console.Write("\nВведите число M -> ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N -> ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int Sum(int m, int n)
+{
+    if (m == n)
+    {
+        return m;
+    }
+    return m + Sum(m + 1, n);
+}
+Console.WriteLine(Sum(M, N));
